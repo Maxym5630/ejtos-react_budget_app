1b6d0a46abab659ff80ff8cfe9a351e10068d000
Your Name <you@example.com>
2023-02-08 11:32:49 -0500

    begin Peer Graded Assignment

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,23 @@
-
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+const Budget = () => {
+    const { budget } = useContext(AppContext);
+    function changeBudget (e) {
+        if (e>20000) {
+            alert('The budget cannot exceed funds £20000');
+        }
+    }
+    return (
+        <div className='alert alert-secondary'>
+            <span>Budget: £
+                <input defaultValue={budget}
+                    type='number'
+                    step='10'
+                    onChange={(event) => changeBudget(event.target.value)}
+                    >
+                    </input>
+                </span>
+        </div>
+    );
+};
+export default Budget;
